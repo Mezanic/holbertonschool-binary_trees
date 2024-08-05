01bdddcabc64bd379c0b0d1ddcd1493fabe5f105
evrade10 <safouevrade@gmail.com>
2024-08-05 12:36:31 +0000

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,166 +1,30 @@
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
 
-#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * struct binary_tree_s - Structure for a binary tree node
+ * struct binary_tree_s - Binary tree node
  *
- * @n: Integer value stored in the node
+ * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
 struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 
-/**
- * binary_tree_node - Creates a new binary tree node
- * @parent: Pointer to the parent node
- * @value: Integer value to store in the new node
- * Return: Pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
-/**
- * binary_tree_insert_left - Inserts a node as the left-child of another node
- * @parent: Pointer to the parent node
- * @value: Integer value to store in the new node
- * Return: Pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-
-/**
- * binary_tree_insert_right - Inserts a node as the right-child of another node
- * @parent: Pointer to the parent node
- * @value: Integer value to store in the new node
- * Return: Pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
-/**
- * binary_tree_delete - Deletes an entire binary tree
- * @tree: Pointer to the root node of the tree to delete
- */
-void binary_tree_delete(binary_tree_t *tree);
-
-/**
- * binary_tree_is_leaf - Checks if a node is a leaf
- * @node: Pointer to the node to check
- * Return: 1 if the node is a leaf, 0 otherwise
- */
-int binary_tree_is_leaf(const binary_tree_t *node);
-
-/**
- * binary_tree_is_root - Checks if a node is the root
- * @node: Pointer to the node to check
- * Return: 1 if the node is the root, 0 otherwise
- */
-int binary_tree_is_root(const binary_tree_t *node);
-
-/**
- * binary_tree_preorder - Traverses a binary tree using pre-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node
- */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-
-/**
- * binary_tree_inorder - Traverses a binary tree using in-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node
- */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-
-/**
- * binary_tree_postorder - Traverses a binary tree using post-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node
- */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure
- * Return: Height of the tree
- */
-size_t binary_tree_height(const binary_tree_t *tree);
-
-/**
- * binary_tree_depth - Measures the depth of a node in a binary tree
- * @tree: Pointer to the node to measure the depth
- * Return: Depth of the node
- */
-size_t binary_tree_depth(const binary_tree_t *tree);
-
-/**
- * binary_tree_size - Measures the size of a binary tree
- * @tree: Pointer to the root node of the tree to measure
- * Return: Size of the tree
- */
-size_t binary_tree_size(const binary_tree_t *tree);
-
-/**
- * binary_tree_leaves - Counts the leaves in a binary tree
- * @tree: Pointer to the root node of the tree to count the leaves in
- * Return: Number of leaves in the tree
- */
-size_t binary_tree_leaves(const binary_tree_t *tree);
-
-/**
- * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
- * @tree: Pointer to the root node of the tree to count the nodes in
- * Return: Number of nodes with at least 1 child in the tree
- */
-size_t binary_tree_nodes(const binary_tree_t *tree);
-
-/**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
- * Return: Balance factor of the tree
- */
-int binary_tree_balance(const binary_tree_t *tree);
-
-/**
- * binary_tree_is_full - Checks if a binary tree is full
- * @tree: Pointer to the root node of the tree to check
- * Return: 1 if the tree is full, 0 otherwise
- */
-int binary_tree_is_full(const binary_tree_t *tree);
-
-/**
- * binary_tree_is_perfect - Checks if a binary tree is perfect
- * @tree: Pointer to the root node of the tree to check
- * Return: 1 if the tree is perfect, 0 otherwise
- */
-int binary_tree_is_perfect(const binary_tree_t *tree);
-
-/**
- * binary_tree_sibling - Finds the sibling of a node
- * @node: Pointer to the node to find the sibling of
- * Return: Pointer to the sibling node, or NULL if no sibling
- */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-
-/**
- * binary_tree_uncle - Finds the uncle of a node
- * @node: Pointer to the node to find the uncle of
- * Return: Pointer to the uncle node, or NULL if no uncle
- */
-binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
-/**
- * binary_tree_print - Prints a binary tree
- * @tree: Pointer to the root node of the tree to print
- */
+/* Function for testing */
 void binary_tree_print(const binary_tree_t *);
 
-#endif /* BINARY_TREE_H */
-
+/* Function for task */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+#endif
