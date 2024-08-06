0644f7641995ad5f2b8cbabcd9991ddd2d4220fe
Mezanic <ezanic.mael@gmail.com>
2024-08-06 15:31:53 +0200

    added 18-binary_tree_uncle for find uncle of a node and update binary_trees.h with the function prototype for it

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,29 @@
+#include "biantry_trees.h"
+
+/**
+ * binary_tree_uncle -  finds the uncle of a node
+ * @node: pointer to the node to find uncle
+ *
+ * Return: Null or Uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	else if (node->parent == NULL)
+		return (NULL);
+
+	else if (node->parent->parent == NULL)
+		return (NULL);
+
+
+	else if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+
+	else if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+
+	else
+		return (NULL);
+}
