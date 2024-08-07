bc884d1acc4c46e397e09a6769b6ed366513cfbd
Mezanic <ezanic.mael@gmail.com>
2024-08-07 10:43:59 +0200

    Add one if condtion for check if the tree is perfect, add on return for use the function recursively

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -44,6 +44,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree_left == 0 && tree_right == 0)
 		return (1);
 
-	return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
+
+	return (0);
 
 }
