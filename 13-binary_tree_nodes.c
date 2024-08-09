6c7bc55da99edbde753eba7ee8c759fcd252a945
Mezanic <ezanic.mael@gmail.com>
2024-08-09 13:11:20 +0200

    Resolve compilation problem for task do by coworker

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,15 +9,10 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-    size_t left_nodes = binary_tree_nodes(tree->left);
-    size_t right_nodes = binary_tree_nodes(tree->right);
-
-    if (tree->left != NULL || tree->right != NULL)
-        return (left_nodes + right_nodes + 1);
-
-    return (left_nodes + right_nodes);
+	return (binary_tree_nodes(tree->left) + 
+		binary_tree_nodes(tree->right) + 1);
 }
 
